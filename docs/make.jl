74a1368a4a45bbdc46c3847728ab442db8f9a8bc
Danny Robinson <dannyrobinson@gmx.com>
2021-02-02 07:16:17 +0000

    Use Actions for docdeploy

--- docs/make.jl
+++ docs/make.jl
@@ -2,4 +2,3 @@ using Documenter
 include("../src/RenoiseOSC.jl")
 
 makedocs(sitename="RenoiseOSC.jl", modules=[RenoiseOSC], pages=["index.md", "api.md", "config.md"])
-# deploydocs("github.com/stellartux/RenoiseOSC.jl.git")
